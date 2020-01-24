435bc6c9cc02f5b14aafdf6cbd3b69905841e581
mohamedmfouad <mohamed.m.fouad.mahmoud@gmail.com>
2020-01-24 07:58:17 +0200

    asking about food

---
+++ askingAboutFood.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Mohammed on 1/24/2020.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout  << "what is your favorite food ?" << endl;
+
+    string food;
+    cin >> food;
+    cout  << " My best food is " << food  << endl;
+
+}
