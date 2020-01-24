277e695089157f5ff9bd11bdada3f7f06d60063e
mohamedmfouad <mohamed.m.fouad.mahmoud@gmail.com>
2020-01-24 07:29:49 +0200

    hellow

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Hello world
+/* by Mohamed Fouad */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout  << "what is your name" << endl;
+
+    string name;
+    cin >> name;
+    cout << name << " Hello world" << endl;
+
+}
